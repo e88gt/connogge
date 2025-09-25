d18133692a1e95daf10a85f74c844a1792566895
e88gt <benshariz.18@gmail.com>
2025-09-25 22:06:29 +0800

    another testing

---
+++ src/main/main.cpp
@@ -0,0 +1,8 @@
+#include "main.hpp"
+
+int main()
+{
+	std::cout << "Does he know\n";
+	
+	return 0;
+}
---
+++ src/main/main.hpp
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+int main();
+
+#endif // _MAIN_H
